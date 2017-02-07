a786cbc99731fe9a54add21e22dc2fa956010409
johnshelb <jsnd11@gmail.com>
2017-02-07 16:28:30 -0500

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,15 @@
+require 'pry'
 def count_elements(array)
-  # code goes here
+  new_hash={}
+  array.map do |string|
+      #binding.pry
+
+      if new_hash.keys.include?(string)
+        new_hash[string]+=1
+      else
+      #binding.pry
+      new_hash[string]=1
+    end
+  end
+  new_hash
 end
- 
